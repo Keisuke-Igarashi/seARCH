ccd36706ba15d1e62e0dfe96466f6bbaaca395e8
Keisuke Igarashi <ke.igarashi@tech.nflabs.jp>
2022-06-09 12:44:50 +0900

    created init.sql

---
+++ Docker/mysql/initdb.d/init.sql
@@ -0,0 +1,48 @@
+DROP DATABASE IF EXISTS majdb;
+CREATE DATABASE majdb;
+USE majdb;
+DROP TABLE IF EXISTS architecture;
+
+CREATE TABLE architecture
+(
+    architecture_id INT(10) NOT NULL AUTOINCREMENT PRIMARY KEY,
+    architecture_name VARCHAR(600) NOT NULL,
+    postalcode INT NOT NULL,
+    address1 VARCHAR(100) NOT NULL,
+    address2 VARCHAR(100) NOT NULL,
+    address3 VARCHAR(100),
+    address4 VARCHAR(100),
+    latitude FLOAT(5,15) NOT NULL,
+    longitude FLOAT(5,15) NOT NULL,
+    architect_id int(10) NOT NULL,
+    createdate DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updatedate DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+
+
+) CHARACTER SET utf8mb4;
+
+CREATE TABLE architect
+(
+    architect_id INT NOT NULL AUTOINCREMENT PRIMARY KEY,
+    architect_name CHAR(100) NOT NULL,
+    createdate DATETIME DEFAULT CURRENT_TIMESTAMP
+
+) CHARACTER SET utf8mb4;
+
+CREATE TABLE favorite
+(
+    favorite_id BIGINT NOT NULL AUTOINCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    architect_id INT NOT NULL,
+    createdate DATETIME DEFAULT CURRENT_TIMESTAMP
+
+) CHARACTER SET utf8mb4;
+
+
+CREATE TABLE user
+(
+    user_id INT NOT NULL AUTOINCREMENT PRIMARY KEY,
+    user_name CHAR(50) NOT NULL,
+    password CHAR(50) NOT NULL
+
+) CHARACTER SET utf8mb4;
