a7077cd19bf328d93752e7f3f57c65bb8feb781d
Keisuke-Igarashi <ke.igarashi@tech.nflabs.jp>
2022-06-09 14:29:57 +0900

    modified init.sql, docker-compose.yml

--- Docker/mysql/initdb.d/init.sql
+++ Docker/mysql/initdb.d/init.sql
@@ -3,45 +3,53 @@ CREATE DATABASE majdb;
 USE majdb;
 DROP TABLE IF EXISTS architecture;
 
+
 CREATE TABLE architecture
 (
-    architecture_id INT(10) NOT NULL AUTOINCREMENT PRIMARY KEY,
-    architecture_name VARCHAR(600) NOT NULL,
+    architecture_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    architecture_name VARCHAR(100) NOT NULL,
     postalcode INT NOT NULL,
     address1 VARCHAR(100) NOT NULL,
     address2 VARCHAR(100) NOT NULL,
     address3 VARCHAR(100),
     address4 VARCHAR(100),
-    latitude FLOAT(5,15) NOT NULL,
-    longitude FLOAT(5,15) NOT NULL,
-    architect_id int(10) NOT NULL,
+    latitude FLOAT(20,15) NOT NULL,
+    longitude FLOAT(20,15) NOT NULL,
+    architect_id INT NOT NULL,
     createdate DATETIME DEFAULT CURRENT_TIMESTAMP,
-    updatedate DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    updatedate DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 
 
 ) CHARACTER SET utf8mb4;
 
+DROP TABLE IF EXISTS architect;
+
+
 CREATE TABLE architect
 (
-    architect_id INT NOT NULL AUTOINCREMENT PRIMARY KEY,
+    architect_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     architect_name CHAR(100) NOT NULL,
     createdate DATETIME DEFAULT CURRENT_TIMESTAMP
 
 ) CHARACTER SET utf8mb4;
 
+DROP TABLE IF EXISTS favorite;
+
 CREATE TABLE favorite
 (
-    favorite_id BIGINT NOT NULL AUTOINCREMENT PRIMARY KEY,
+    favorite_id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     user_id INT NOT NULL,
     architect_id INT NOT NULL,
     createdate DATETIME DEFAULT CURRENT_TIMESTAMP
 
 ) CHARACTER SET utf8mb4;
 
+DROP TABLE IF EXISTS user;
+
 
 CREATE TABLE user
 (
-    user_id INT NOT NULL AUTOINCREMENT PRIMARY KEY,
+    user_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     user_name CHAR(50) NOT NULL,
     password CHAR(50) NOT NULL
 
