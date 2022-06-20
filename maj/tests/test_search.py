d3553aa793fd9ab0789a0f7a2ccd8da8b8b415bc
Keisuke-Igarashi <igarashi4948@gmial.com>
2022-06-20 12:40:31 +0900

    created test_search.py

--- maj/tests/test_search.py
+++ maj/tests/test_search.py
@@ -1,5 +1,43 @@
-from maj.majapp import search
+import requests
+import sqlite3
+import pytest
 
-def test_search():
+from majapp import create_app
+from majapp.db import get_db
+# from majapp import search
+# from flask import (
+#     request
+# )
 
+# flaskインスタンスの作成
+@pytest.fixture
+def app():
+    app = create_app({'TESTING': True})
+
+    return app
+
+
+# test実行用clientの作成
+@pytest.fixture
+def client(app):
+
+    return app.test_client()
+
+# def search(client, architecture_name):
+#     return client.post('/search')
+
+
+def test_search(client):
+
+    # response = client.get('/hello')
+    # assert response.data ==  b'Hello, World!'
+
+    response = client.get('/search')
+    assert b'seARCH' in response.data
+
+    # response = client.post('/search', data = {'architect_name':''})
+    # assert b'Hikari' in response.data
+
+    response = client.post('/search', data = {'architect_name': 'tadao'})
+    assert b'Hikari' in response.data
     
